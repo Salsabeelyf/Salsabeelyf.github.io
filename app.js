b8778dca2d8c67a0ab67cb661943c7959a4e003e
Salsabeel Salah <salsabeelsay@gmail.com>
2017-12-21 14:16:17 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+var about = document.getElementById("about-link");
+var port = document.getElementById("portfolio-link");
+var contact = document.getElementById("contact-link");
+
+var clickfn = function(){
+    if ( document.URL.includes("#about")) {
+        if(!about.className.includes("current")){
+            about.className += " current";
+        }
+        if(port.className.includes("current")){
+            port.className = "nav-right";
+        }
+        if(contact.className.includes("current")){
+            contact.className = "nav-right";
+        }
+    }else if( document.URL.includes("#portfolio")) { 
+        if(about.className.includes("current")){
+            about.className = "nav-right";
+        }
+        if(!port.className.includes("current")){
+            port.className += " current";
+        }
+        if(contact.className.includes("current")){
+            contact.className = "nav-right";
+        }
+    }
+    else if ( document.URL.includes("#contact")) {
+        if(about.className.includes("current")){
+            about.className = "nav-right";
+        }
+        if(port.className.includes("current")){
+            port.className = "nav-right";
+        }
+        if(!contact.className.includes("current")){
+            contact.className += " current";
+        }
+    }
+};
+
+window.onscroll = function()
+{
+    if(document.body.scrollTop > 1600 || document.documentElement.scrollTop > 1600){
+        if(about.className.includes("current")){
+            about.className = "nav-right";
+        }
+        if(port.className.includes("current")){
+            port.className = "nav-right";
+        }
+        if(!contact.className.includes("current")){
+            contact.className += " current";
+        }
+   }
+    else if(document.body.scrollTop > 400 || document.documentElement.scrollTop > 400){
+        if(about.className.includes("current")){
+            about.className = "nav-right";
+        }
+        if(!port.className.includes("current")){
+            port.className += " current";
+        }
+        if(contact.className.includes("current")){
+            contact.className = "nav-right";
+        }
+    }
+    else if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0)
+    {
+        if(!about.className.includes("current")){
+            about.className += " current";
+        }
+        if(port.className.includes("current")){
+            port.className = "nav-right";
+        }
+        if(contact.className.includes("current")){
+            contact.className = "nav-right";
+        }
+    }
+
+}
